bed43a93c51d10f5e4acef4cb2636455a9d546bb
Alecs Geuder <alecs.geuder@moreassociates.com>
2011-11-30 19:07:46 +0000

    fixturesimport, querying, readme update

---
+++ cities/management/commands/fixtures_to_mongo.py
@@ -0,0 +1,114 @@
+import os
+import re
+import sys
+
+from django.core.management.base import BaseCommand
+from django.core.management.commands.loaddata import Command as LoadCommand
+from django.utils import simplejson
+
+from mongoengine import *
+from mongoengine.queryset import OperationError
+from mongoengine.base import ValidationError
+
+from cities.models import *
+
+
+class Extracter(object):
+
+    MODELS = {
+        'cities.country': Country,
+        'country': Country,
+        'cities.region': Region,
+        'region': Region,
+        'cities.city': City,
+        'city': City,
+        'cities.district': District,
+        'district': District,
+    }
+
+    def __init__(self, country_codes=None, verbosity=None, fixtures_path=None):
+        self.countries = ([code.upper() for code in country_codes]
+                          if country_codes else (()))
+        if not fixtures_path:
+            fixtures_dir = '%s/../../fixtures' % os.path.dirname(__file__)
+            fixtures_path = '%s/geonames_dump.json' % fixtures_dir
+        self.src_data = simplejson.load(open(fixtures_path), 'cp1252')
+        # decimal degrees in (longitude, latitude) order - same as for GeoJSON
+        # --> http://geojson.org/geojson-spec.html
+        self.lonlat = re.compile('(?P<lon>-?\d+\.\d+) (?P<lat>-?\d+\.\d+)')
+        self.imported_countries, self.imported_country_ids = [], []
+        self.imported_city_ids = []
+        self.verbosity = int(verbosity) if verbosity else 0
+        self.old_model_class = None
+
+    def to_db(self):
+        for model in self.src_data:
+            pk, model_class = model['pk'], self.MODELS[model['model']]
+            if self.verbosity >= 1 and model_class != self.old_model_class:
+                print 'starting to import %s' % model_class
+            self.old_model_class = model_class
+            if not self.country_in_wishlist(model):
+                if self.verbosity >= 2:
+                    print 'skipping %s %s' % (model_class.__name__,
+                                              model['fields']['name'])
+                continue
+            new_instance = model_class(id=pk)
+            for field, value in model['fields'].iteritems():
+                if field == 'location':
+                    match = self.lonlat.search(value)
+                    # preserving ((lon, lat)) order
+                    value = ((
+                        float(match.group('lon')),
+                        float(match.group('lat')),
+                    ))
+                elif isinstance(getattr(model_class, field), ReferenceField):
+                    reference_class = self.MODELS[field]
+                    value = reference_class.objects.get(id=value)
+                setattr(new_instance, field, value)
+            new_instance.save()
+            print "Saved %s %s" % (model_class.__name__, new_instance)
+            if model_class == Country:
+                self.imported_countries.append(new_instance)
+                self.imported_country_ids.append(pk)
+            elif model_class == City:
+                self.imported_city_ids.append(model)
+
+    def country_in_wishlist(self, model):
+        model_class, code = model['model'], model['fields'].get('code')
+        countries = self.imported_countries # these are Country instances
+        if self.countries:
+            if model_class == 'cities.country':
+                if code not in self.countries:
+                    return False
+            elif model_class == 'cities.region':
+                country = model['fields']['country'] # this is a pk
+                if country not in self.imported_country_ids:
+                    return False
+            elif model_class == 'cities.city':
+                region = Region.objects(id=model['fields']['region'])
+                if not region or region[0].country not in countries:
+                    return False
+            else:
+                assert model_class == 'cities.district'
+                city = model['fields'].get('city') # this is a pk
+                if city and not city in self.imported_city_ids:
+                    return False
+        # if not self.countries, all are in wishlist
+        return True
+
+
+class Command(BaseCommand):
+
+    help = 'Import selected countries into database.'
+    args = "code [code ...]"
+    option_list = LoadCommand.option_list
+
+    def handle(self, *args, **options):
+        if not len(args):
+            self.stderr.write('No countries specified. This will import all fixtures.')
+            ans = raw_input("Continue Y/N: ")
+            if ans.lower() not in ["y", "yes"]:
+                sys.exit()
+        country_codes = args[0].split(',') if args else None
+        extracter = Extracter(country_codes, verbosity=options['verbosity'])
+        extracter.to_db()
--- cities/models.py
+++ cities/models.py
@@ -1,1 +1,148 @@
-models_mongo.py
+from math import radians, sin, cos, asin, sqrt
+
+from mongoengine import *
+
+
+class Country(Document):
+    id = IntField(primary_key=True)
+    name = StringField(max_length = 200)
+    code = StringField(max_length = 2, unique=True)
+    population = IntField()
+    continent = StringField(max_length = 2)
+    tld = StringField(max_length = 5, unique=True)
+
+    meta = {
+        'indexes': ['code'],
+        'ordering': ['name'],
+    }
+
+    def __repr__(self):
+        return unicode(self).encode('utf8')
+
+    def __unicode__(self):
+        return self.name
+
+    @property
+    def hierarchy(self):
+        return [self]
+
+
+class Region(Document):
+    id = IntField(primary_key=True)
+    name = StringField(max_length = 200)
+    slug = StringField(max_length = 200)
+    code = StringField(max_length = 10)
+    country = ReferenceField(Country)
+
+    meta = {
+        'indexes': ['slug', 'code'],
+    }
+
+    def __repr__(self):
+        return unicode(self).encode('utf8')
+
+    def __unicode__(self):
+        return "%s, %s" % (self.name, self.country)
+
+    @property
+    def hierarchy(self):
+        list = self.country.hierarchy
+        list.append(self)
+        return list
+
+
+class GeoMixin(object):
+
+    """
+    Some custom query methods on top of the ones implemented in mongoengine
+
+    NOTE the order ((latitude, longitude))!
+    """
+
+    @classmethod
+    def nearest(cls, location):
+        longitute, latitude = location
+        return cls.near(lon, lat).first()
+
+    @classmethod
+    def near(cls, location):
+        """
+        location == (( longitude, latitude ))
+        """
+        return cls.objects.filter(location__near=location)
+
+    def distance_to(self, other):
+        """
+        Using the haversine formula. Mongo-db should do that already -
+        How to query using mongo-dbs powers??
+        """
+        if isinstance(other, (tuple, list)):
+            lon, lat = other
+        else: # if !tuple we expect an object with a geopoint field 'location'
+            lon, lat = other.location[0], other.location[1]
+        diff_lon = radians(self.location[0] - lon)
+        diff_lat = radians(self.location[1] - lat)
+        lat1, lat2 = radians(self.location[1]), radians(lat)
+
+        haversine_lat = sin(diff_lat / 2.0) ** 2
+        haversine_lon = sin(diff_lon / 2.0) ** 2
+        a = haversine_lat + cos(lat1) * cos(lat2) * haversine_lon
+
+        great_circle_distance = 2 * asin(min(1,sqrt(a)))
+
+        earth_radius = 6378 # in km
+        distance = earth_radius * great_circle_distance
+
+        return distance
+
+
+class City(Document, GeoMixin):
+    id = IntField(primary_key=True)
+    name = StringField(max_length = 200)
+    slug = StringField(max_length = 200)
+    region = ReferenceField(Region)
+    country = ReferenceField(Country) # added for country rel. search
+    location = GeoPointField()
+    population = IntField()
+
+    meta = {
+        'indexes': ['slug'],
+    }
+
+    def __repr__(self):
+        return unicode(self).encode('utf8')
+
+    def __unicode__(self):
+        return "%s, %s, %s" % (self.name, self.region.name,
+                               self.region.country.name)
+
+    @property
+    def hierarchy(self):
+        list = self.region.hierarchy
+        list.append(self)
+        return list
+
+    def save(self, *args, **kwargs):
+        self.country = self.country or self.region.country
+        super(City, self).save(*args, **kwargs)
+
+class District(Document, GeoMixin):
+    id = IntField(primary_key=True)
+    name = StringField(max_length = 200)
+    slug = StringField(max_length = 200)
+    city = ReferenceField(City)
+    location = GeoPointField()
+    population = IntField()
+
+    meta = {
+        'indexes': ['slug'],
+    }
+
+    def __unicode__(self):
+        return u"%s, %s" % (self.name, self.city)
+
+    @property
+    def hierarchy(self):
+        list = self.city.hierarchy
+        list.append(self)
+        return list
